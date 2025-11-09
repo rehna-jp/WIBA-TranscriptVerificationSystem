04ad2e53e995e9550fd49a632ca7ff595b905c18
rehna-jp <jeremyrehna05@gmail.com>
2025-11-09 19:07:30 +0000

    initial commit, worked on the contracts

---
+++ src/institutionRegistry.sol
@@ -0,0 +1,96 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+contract institutionRegistry{
+
+    address immutable i_admin;
+
+    constructor (address admin){
+      i_admin = admin;
+    }
+    
+    struct institution{
+        uint id;
+        address walletAddress;
+        string name;
+        string country;
+        string accreditedURL;
+        bool isVerified;
+        uint256 dateRegistered;
+        string email;
+    }
+   mapping(address => institution) institutions;
+   uint256 public numberOfInstitutions;
+   uint256 public numberOfVerifiedInstitutions;
+
+   event InstitutionRegistered(address indexed _walletAddress, string _name, string _country, string _accreditedURL, string email);
+   event InstitutionVerified(address indexed _walletAddress, string _name, string _country, string _accreditedURL, string email);
+   event InstitutionSuspended(address indexed _walletAddress, string _name, string _country, string _accreditedURL, string email);
+
+   modifier onlyAdmin(){
+        require(msg.sender == i_admin, "Only the admin can call this function");
+        _;
+   }
+
+   error InstitutionDoesNotExist();
+   error InstitutionAlreadyVerified();
+   error InstitutionAlreadyRegistered();
+   
+   function registerInstitution(string memory _name, string memory _country, string memory _accreditedURL,  string memory email) external {
+        if (institutions[msg.sender].walletAddress != address(0)) {
+            revert InstitutionAlreadyRegistered();
+        }
+        numberOfInstitutions++;
+        institutions[msg.sender] = institution({
+            id: numberOfInstitutions,
+            walletAddress: msg.sender,
+            name: _name,
+            country: _country,
+            accreditedURL: _accreditedURL,
+            isVerified: false,
+            dateRegistered: block.timestamp,
+            email: email
+        } );
+
+        emit InstitutionRegistered(msg.sender, _name, _country, _accreditedURL, email);
+   }
+
+   function VerifyInstitution(address _institutionAddress) external onlyAdmin {
+      if (institutions[_institutionAddress].walletAddress == address(0)) {
+           revert InstitutionDoesNotExist();
+      }
+      if (institutions[_institutionAddress].isVerified == true)  {
+         revert InstitutionAlreadyVerified();
+      }
+
+      institution memory inst = institutions[_institutionAddress];
+
+      numberOfVerifiedInstitutions++;
+      institutions[_institutionAddress].isVerified = true;
+      emit InstitutionVerified(_institutionAddress, inst.name, inst.country, inst.accreditedURL, inst.email);
+   }
+
+   function suspendInstitution(address _institutionAddress) external onlyAdmin {
+       institutions[_institutionAddress].isVerified = false;
+       numberOfVerifiedInstitutions--;
+
+       institution memory inst = institutions[_institutionAddress];
+       emit InstitutionSuspended(_institutionAddress, inst.name, inst.country, inst.accreditedURL, inst.email);
+
+   }
+
+   function isInstitutionVerified(address _institutionAddress) external view returns (bool) { 
+       if (institutions[_institutionAddress].walletAddress == address(0)) {
+           revert InstitutionDoesNotExist();
+      }  
+       return institutions[_institutionAddress].isVerified;
+   }
+
+   
+   function getInstitutionDetais(address _institutionAddress) external view returns(institution memory){
+        if (institutions[_institutionAddress].walletAddress == address(0)) {
+           revert InstitutionDoesNotExist();
+      }  
+      return institutions[_institutionAddress];
+   }
+}
---
+++ src/transcriptVerification.sol
@@ -0,0 +1,113 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import {institutionRegistry} from './institutionRegistry.sol';
+
+contract TranscriptManager{
+     institutionRegistry public registry;
+     constructor (address _registryAddress) {
+         registry = institutionRegistry(_registryAddress);
+     }
+
+     error TranscriptDoesNotExist();
+
+     enum status{ACTIVE, REVOKED}
+     enum degreeType {ASSOCIATE, BACHELOR, MASTER, DOCTORATE, CERTIFICATE, DIPLOMA, POSTDOCTORATE}
+
+     struct Transcript {
+        uint id;
+        string studentId;   
+        address issuedBy;
+        bytes32 documenthash;
+        degreeType degreeType;
+        uint256 dateIssued;
+        string ipfscid;
+        address studentAddress;
+        status status;
+        uint256 graduationyear;
+     }
+
+     uint public transcriptCount;
+
+     mapping (uint => Transcript) public transcripts;
+     mapping(string => bool) public existingCIDs;
+     mapping (string => uint) cidToTranscriptId;
+     mapping (address => uint[]) studentToTranscript;
+
+
+     event TranscriptIssued(address indexed studentAddress,string ipfsCid,uint256 dateissued, address issuedby);
+     event TranscriptInvalidated(address indexed studentAddress, uint256 dateissued, address issuedby);
+     
+     modifier onlyVerifiedInstitution{
+        require(registry.isInstitutionVerified(msg.sender), "Only verified institutions can issue transcripts");
+        _;
+     }
+
+     function issueTranscripts(
+        string memory studentId, 
+        string memory cid, 
+         bytes32 documenthash, 
+        degreeType degreetype,
+        address studentAddress,
+        uint256 graduationyear
+        ) external onlyVerifiedInstitution{
+            require(!existingCIDs[cid], "CID already used for another transcript");
+
+            transcriptCount++;
+            Transcript memory t = Transcript({
+                 id: transcriptCount,
+                 studentId: studentId,
+                 issuedBy: msg.sender,
+                 documenthash: documenthash,
+                 degreeType: degreetype,
+                 dateIssued: block.timestamp,
+                 ipfscid: cid,
+                 studentAddress: studentAddress,
+                 status: status.ACTIVE,
+                 graduationyear: graduationyear
+             });
+            transcripts[transcriptCount] = t;
+
+            existingCIDs[cid] = true;
+            cidToTranscriptId[cid] = transcriptCount;
+            studentToTranscript[studentAddress].push(transcriptCount);
+            emit TranscriptIssued(studentAddress,cid, block.timestamp, msg.sender);
+
+     }
+     
+     function verifyTranscript(string memory cid) external view returns(Transcript memory){
+          uint id = cidToTranscriptId[cid];
+         if (id == 0 || transcripts[id].status != status.ACTIVE ) {
+            revert TranscriptDoesNotExist();
+         }
+
+         return transcripts[id];
+
+     }
+
+     function inValidateTranscript(uint256 transcriptId) external onlyVerifiedInstitution{
+         Transcript storage t = transcripts[transcriptId];
+        require(transcripts[transcriptId].issuedBy == msg.sender, "Only the issuer can invalidate the transcript");
+        require(transcripts[transcriptId].status == status.ACTIVE, "Transcript is already invalidated");
+        require(t.studentAddress != address(0), "Transcript does not exist");
+        t.status = status.REVOKED;
+        emit TranscriptInvalidated(t.studentAddress, block.timestamp, msg.sender);
+     }
+
+     function getTranscriptDetails(uint256 transcriptId) external view returns (Transcript memory){
+         require(transcripts[transcriptId].status == status.ACTIVE, "Transcript is invalidated");
+         Transcript memory t = transcripts[transcriptId];
+         return t;
+     }
+
+    function getStudentTranscripts(address student) external view returns (Transcript[] memory) {
+    uint[] memory ids = studentToTranscript[student];
+    Transcript[] memory result = new Transcript[](ids.length);
+    for (uint i = 0; i < ids.length; i++) {
+        result[i] = transcripts[ids[i]];
+    }
+    return result;
+}
+
+
+}
